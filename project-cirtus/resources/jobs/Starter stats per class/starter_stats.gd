909f19939415913296f11931972a6360b9f8bd9e
Jr <mmurillo9708@panther.chaffey.edu>
2024-11-18 19:58:49 -0800

    resources for starter stats

---
+++ project-cirtus/resources/jobs/Starter stats per class/starter_stats.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+class_name starter_stats
+
+@export var job_name : String = "Job"
+
+@export var max_hp : int
+@export var max_mp : int
+@export var speed: int
+@export var defense: int
+@export var strength: int
