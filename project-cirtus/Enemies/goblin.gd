164c9866ef1ccb2b6f0ddd652961bb75abb4ee38
Student BE106 <Student.BE106@chaffey.edu>
2024-11-21 16:45:39 -0800

    pluh

--- project-cirtus/Enemies/goblin.gd
+++ project-cirtus/Enemies/goblin.gd
@@ -37,3 +37,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	fight()
+
+
+func _on_area_3d_area_exited(area: Area3D) -> void:
+	pass 
--- project-cirtus/player.gd
+++ project-cirtus/player.gd
@@ -1,25 +1,34 @@
 extends CharacterBody3D
 
 @export var sprite: AnimatedSprite3D
+@export var timerP:Timer
+
 
 var idle_anim:String = "Idle Right"
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+
+var fighting = false
 var in_shadow:bool = false
 var shadow_timer: float = 0.0
 var lock = false
 var can_interact: bool = false
 var postionL
-func fight(postion):
+func postioning(postion):
 	lock = true
 	postionL = postion
 	print("connecting")
 
+func fight():
+	fighting = true
+	lock = false
+
+
 
 func _ready() -> void:
 	#Dialogic.signal_event.connect(_on_dialogic_signal)
-	SignalBus.connect("fighting", fight)
+	SignalBus.connect("fighting", postioning)
 func _process(delta: float) -> void:
 	if in_shadow:
 		shadow_timer += delta
@@ -30,8 +39,17 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	
 	if lock:
-		global_position.x = move_toward(global_position.x,postionL.x,delta * 2)
-		global_position.z = move_toward(global_position.z,postionL.z,delta * 2)
+		
+		global_position.x = move_toward(global_position.x,postionL.x,delta * 20)
+		global_position.z = move_toward(global_position.z,postionL.z,delta * 20)
+		print("global postion")
+		print(global_position)
+		print("postion Left")
+		print(postionL)
+		
+		if global_position.x == postionL.x && global_position.z == postionL.z:
+			print("pluh")
+			fight()
 		
 	
 	# Add the gravity.
@@ -39,14 +57,17 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("Jump") and is_on_floor():
+		#elocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("Left", "Right", "Up", "Down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	
+	
+	
+	if direction && not fighting:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		if direction.x>0:
@@ -77,3 +98,8 @@ func _input(event: InputEvent) -> void:
 func _on_dialogic_signal(argument: String):
 	if argument == "its a signal":
 		print("A signal was fired")
+
+
+func _on_timer_timeout() -> void:
+	fight()
+	print("pluh")
