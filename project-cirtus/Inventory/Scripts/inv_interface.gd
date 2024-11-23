255f47eea7ce9c5284ea5b9a2120bad26cc048c0
Jr <mmurillo9708@panther.chaffey.edu>
2024-11-22 16:29:49 -0800

    is this working yet? no

--- project-cirtus/Inventory/Scripts/inv_interface.gd
+++ project-cirtus/Inventory/Scripts/inv_interface.gd
@@ -1,9 +1,16 @@
 extends Control
+
+var grabbed_slot_data: SlotData
+
 @onready var player_inventory: PanelContainer = $"Player Inventory"
 
 func set_player_inventory_data(inventory_data: InventoryData) -> void:
+	inventory_data.inventory_interact.connect(on_inventory_interact)
 	player_inventory.set_inventory_data(inventory_data)
 	pass
 
 func on_inventory_interact(inventory_data: InventoryData, index:int, button:int) -> void:
-	pass
+	match[grabbed_slot_data, button]: 
+		[null, MOUSE_BUTTON_LEFT]:
+			grabbed_slot_data = inventory_data.grab_slot_data(index)
+	print(grabbed_slot_data)
