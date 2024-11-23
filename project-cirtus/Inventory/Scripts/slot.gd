2e6eca8dd0d572b3c3f86f14c5c5aca650bb9c4b
Jr <mmurillo9708@panther.chaffey.edu>
2024-11-22 17:52:46 -0800

    skibidi

--- project-cirtus/Inventory/Scripts/slot.gd
+++ project-cirtus/Inventory/Scripts/slot.gd
@@ -8,7 +8,7 @@ signal slot_clicked(index:int, button: int)
 func set_slot_data(slot_data: SlotData) -> void:
 	var item_data = slot_data.item_data
 	texture_rect.texture = item_data.item_texture
-	tooltip_text = "$s \n $s" % [item_data.item_name, item_data.item_description]
+	tooltip_text = "%s\n%s" % [item_data.item_name, item_data.item_description]
 	
 	if slot_data.quantity >1:
 		quantity_label.text = "x%s" % slot_data.quantity
